335e1011b4bef55b70ebbfa5e065b14064abaefe
Jeevan <95750003+anahgem@users.noreply.github.com>
2022-01-24 16:09:03 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -39,18 +39,10 @@ class Speedometer extends Component {
           <h1 className="speed-text">Speed is {speed}mph</h1>
           <p className="speed-limits">Min Limit is 0mph, Max Limit is 200mph</p>
           <div className="buttons-container">
-            <button
-              type="button"
-              className="accelerate-button button"
-              onClick={this.onClickAccelerateButton}
-            >
+            <button type="button" className="accelerate-button button" onClick={this.onClickAccelerateButton} >
               Accelerate
             </button>
-            <button
-              type="button"
-              className="apply-brake-button button"
-              onClick={this.onClickApplyBrakeButton}
-            >
+            <button type="button" className="apply-brake-button button" onClick={this.onClickApplyBrakeButton} >
               Apply Brake
             </button>
           </div>
